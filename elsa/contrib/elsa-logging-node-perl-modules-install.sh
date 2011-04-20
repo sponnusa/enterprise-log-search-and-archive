8aaccbcf2bd5bc60f403608ddee797f427d3d632
mcholste <mcholste@gmail.com>
2011-04-20 16:26:10 +0000

    Updated INSTALL with more instructions.  Added many comments to elsa.conf.  Updated install scripts to hard-fail on CPAN failures.

--- elsa/contrib/elsa-logging-node-perl-modules-install.sh
+++ elsa/contrib/elsa-logging-node-perl-modules-install.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
-cpanm YAML::Syck
-cpanm Moose
-cpanm IO::Socket
-cpanm Data::Serializer
-cpanm POE::Event::Message
-cpanm Config::JSON
-cpanm Exception::Class
-cpanm POE::Component::PreforkDispatch
-cpanm Date::Manip
-cpanm DBD::mysql
-cpanm Sphinx::Config
-cpanm -f Sphinx::Search
-cpanm Sys::Info
-cpanm Sys::MemInfo
-cpanm String::CRC32
-cpanm Search::QueryParser
-cpanm Search::QueryParser::SQL
-cpanm Net::Server::Daemonize
-cpanm Log::Log4perl
-cpanm POE::API::Peek
-cpanm Parallel::ForkManager
-cpanm Sort::Key
-cpanm Sys::Hostname::FQDN
-cpanm Net::DNS
-cpanm EV
+cpanm YAML::Syck &&
+cpanm Moose &&
+cpanm IO::Socket &&
+cpanm Data::Serializer &&
+cpanm POE::Event::Message &&
+cpanm Config::JSON &&
+cpanm Exception::Class &&
+cpanm POE::Component::PreforkDispatch &&
+cpanm Date::Manip &&
+cpanm DBD::mysql &&
+cpanm Sphinx::Config &&
+cpanm -f Sphinx::Search &&
+cpanm Sys::Info &&
+cpanm Sys::MemInfo &&
+cpanm String::CRC32 &&
+cpanm Search::QueryParser &&
+cpanm Search::QueryParser::SQL &&
+cpanm Net::Server::Daemonize &&
+cpanm Log::Log4perl &&
+cpanm POE::API::Peek &&
+cpanm Parallel::ForkManager &&
+cpanm Sort::Key &&
+cpanm Sys::Hostname::FQDN &&
+cpanm Net::DNS &&
+cpanm EV &&
 cpanm File::Slurp
--- elsa/contrib/elsa-web-frontend-perl-modules-install.sh
+++ elsa/contrib/elsa-web-frontend-perl-modules-install.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
-cpanm YAML::Syck
-cpanm Moose
-cpanm IO::Socket
-cpanm Data::Serializer
-cpanm POE::Event::Message
-cpanm Config::JSON
-cpanm Net::LDAP::Express
-cpanm Net::LDAP::FilterBuilder
-cpanm Module::Pluggable
-cpanm URI::Escape
-cpanm DBD::mysql
-cpanm POE::Filter::Reference
-cpanm Digest::HMAC_SHA1
-cpanm Mail::Internet
-cpanm File::Slurp
-cpanm MIME::Base64
-cpanm EV
-cpanm Time::HiRes
-cpanm Plack::Builder
-cpanm Plack::Session
-cpanm Plack::Middleware::CrossOrigin
-cpanm Plack::Middleware::ForwardedHeaders
+cpanm YAML::Syck &&
+cpanm Moose &&
+cpanm IO::Socket &&
+cpanm Data::Serializer &&
+cpanm POE::Event::Message &&
+cpanm Config::JSON &&
+cpanm Net::LDAP::Express &&
+cpanm Net::LDAP::FilterBuilder &&
+cpanm Module::Pluggable &&
+cpanm URI::Escape &&
+cpanm DBD::mysql &&
+cpanm POE::Filter::Reference &&
+cpanm Digest::HMAC_SHA1 &&
+cpanm Mail::Internet &&
+cpanm File::Slurp &&
+cpanm MIME::Base64 &&
+cpanm EV &&
+cpanm Time::HiRes &&
+cpanm Plack::Builder &&
+cpanm Plack::Session &&
+cpanm Plack::Middleware::CrossOrigin &&
+cpanm Plack::Middleware::ForwardedHeaders &&
 # For auth plugins (optional, but enabled by default)
-cpanm Authen::Simple::PAM
-cpanm Authen::Simple::LDAP
+cpanm -n Authen::Simple::PAM &&
+cpanm -n Authen::Simple::LDAP &&
 # For plugins (optional, but enabled by default)
-cpanm Module::Install
-cpanm PDF::API2::Simple
-cpanm XML::Writer
-cpanm Spreadsheet::WriteExcel
+cpanm Module::Install &&
+cpanm PDF::API2::Simple &&
+cpanm XML::Writer &&
+cpanm Spreadsheet::WriteExcel &&
 cpanm Parse::Snort
