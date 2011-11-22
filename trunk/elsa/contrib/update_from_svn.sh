2210550aeb2b308790f65a48ece5ab2df5e6253b
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2011-11-22 20:22:32 +0000

    Updated update_from_svn.sh script.

--- trunk/elsa/contrib/update_from_svn.sh
+++ trunk/elsa/contrib/update_from_svn.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-svn export --force https://enterprise-log-search-and-archive.googlecode.com/svn/trunk/elsa /usr/local/elsa
+
+# EDIT CONFIG VARIABLES
+BASE_DIR="/usr/local"
+DATA_DIR="/data"
+TMP_DIR="/tmp"
+
+svn export --force https://enterprise-log-search-and-archive.googlecode.com/svn/trunk/elsa "$BASE_DIR/elsa"
+if [ $? -ne 0 ]; then
+	echo "Error updating from svn"
+	exit
+fi
+
+if [ -f /etc/elsa_web.conf ]; then
+	service apache2 restart
+fi
+
+if [ -f /etc/elsa_node.conf ]; then
+	service syslog-ng restart
+fi
