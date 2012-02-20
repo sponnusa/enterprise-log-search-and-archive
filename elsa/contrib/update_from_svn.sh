306f4ce409386effa36ce644b5815f9a73be3a81
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2012-02-20 23:50:23 +0000

    Added deprecated message to update_from_svn.sh as install.sh should now be used.

--- elsa/contrib/update_from_svn.sh
+++ elsa/contrib/update_from_svn.sh
@@ -1,20 +1,2 @@
 #!/bin/sh
-
-# EDIT CONFIG VARIABLES
-BASE_DIR="/usr/local"
-DATA_DIR="/data"
-TMP_DIR="/tmp"
-
-svn export --force https://enterprise-log-search-and-archive.googlecode.com/svn/trunk/elsa "$BASE_DIR/elsa"
-if [ $? -ne 0 ]; then
-	echo "Error updating from svn"
-	exit
-fi
-
-if [ -f /etc/elsa_web.conf ]; then
-	service apache2 restart
-fi
-
-if [ -f /etc/elsa_node.conf ]; then
-	service syslog-ng restart
-fi
+echo "Please use 'install.sh node update' and 'install.sh web update' as this script is now deprecated." 
