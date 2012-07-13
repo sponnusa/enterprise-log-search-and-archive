7c97f3af42b0a6939be7feed272b7761e1cc0889
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2012-07-13 17:45:43 +0000

    Added usage and other niceties to cli.pl.

--- trunk/elsa/web/cli.pl
+++ trunk/elsa/web/cli.pl
@@ -7,7 +7,7 @@ use FindBin;
 use lib $FindBin::Bin . '/lib';
 use API;
 
-my $config_file = -f '/etc/elsa.conf' ? '/etc/elsa.conf' : '/usr/local/elsa/etc/elsa.conf';
+my $config_file = -f '/etc/elsa_web.conf' ? '/etc/elsa_web.conf' : '/usr/local/elsa/etc/elsa_web.conf';
 if ($ENV{ELSA_CONF}){
 	$config_file = $ENV{ELSA_CONF};
 }
@@ -17,12 +17,18 @@ getopts('f:q:c:', \%opts);
 if ($opts{c}){
 	$config_file = $opts{c};
 }
+die 'Invalid config file given' unless -f $config_file;
+my $query = $opts{q};
+unless (defined $query){
+	$query = join(' ', @ARGV);
+}
+die usage() unless defined $query;
 
 my $start = time();
 my $api = API->new(config_file => $config_file);
 my $user = User->new(conf => $api->conf, username => 'system');
 
-my $q = $api->query({query_string => $opts{q}, user => $user});
+my $q = $api->query({query_string => $query, user => $user});
 my $duration = time() - $start;
 exit unless $q->results->total_records;
 my $format = $opts{f} ? $opts{f} : 'tsv';
@@ -33,3 +39,12 @@ if ($q->has_warnings){
 }
 print $api->format_results({ results => $q->results->results, format => $format }) . "\n";
 print "Finished in $duration seconds.\n";
+
+sub usage {
+	return q{
+usage: -q <query string>
+       -c <web config file; default is /etc/elsa_web.conf, alternatively you can set env var ELSA_CONF>
+       -f <format, default tsv; only other choice is json>
+If called without arguments, all arguments are interpreted as the query and defaults are used.
+};
+}
