f789bb23f14cc8ba3d67ab9858b89232a2024b67
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2012-08-28 14:20:21 +0000

    Added "name" attr to query input field to be more browser-friendly (#66).

--- elsa/web/inc/main.js
+++ elsa/web/inc/main.js
@@ -418,7 +418,7 @@ YAHOO.ELSA.main = function () {
 		oQueryFormGridCfg['grid'] = [
 			[ 
 				{type:'text', args:'Query'},
-				{type:'input', args:{id:'q', size:80} },
+				{type:'input', args:{id:'q', name:'elsa_query_box', size:80} },
 				{type:'widget', className:'Button', args:oSubmitButtonConfig},
 				{type:'element', element:'a', args:{href:'http://code.google.com/p/enterprise-log-search-and-archive/wiki/Documentation#Queries', innerHTML:'Help', target:'_new'}}
 			]
