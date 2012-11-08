36a0308dea7b2fea903fb970b1b965b1fb009cc1
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2012-11-08 17:42:28 +0000

    Fallback to GeoIP country database if city database is unavailable.

--- trunk/elsa/web/lib/Transform/GeoIP.pm
+++ trunk/elsa/web/lib/Transform/GeoIP.pm
@@ -11,21 +11,39 @@ has 'name' => (is => 'rw', isa => 'Str', required => 1, default => $Name);
 sub BUILD {
 	my $self = shift;
 	
-	my $geoip = Geo::IP->open_type(GEOIP_CITY_EDITION_REV1, GEOIP_MEMORY_CACHE) or die('Unable to create GeoIP object: ' . $!);
+	my $geoip;
+	my $cc_only = 0;
+	eval {
+		$geoip = Geo::IP->open_type(GEOIP_CITY_EDITION_REV1, GEOIP_MEMORY_CACHE) or die('Unable to create GeoIP object: ' . $!);
+	};
+	if ($@){
+		$self->log->warn('GeoIP city edition not found, falling back to country edition');
+		$geoip = Geo::IP->open_type(GEOIP_COUNTRY_EDITION, GEOIP_MEMORY_CACHE) or die('Unable to create GeoIP object: ' . $!);
+		$cc_only = 1;
+	}
 	
 	foreach my $datum (@{ $self->data }){
 		$datum->{transforms}->{$Name} = {};
 		
 		foreach my $key (keys %{ $datum }){
 			if ($key eq 'srcip' or $key eq 'dstip' or $key eq 'ip'){
-				my $record = $geoip->record_by_addr($datum->{$key});
-				next unless $record;
-				$datum->{transforms}->{$Name}->{$key} = {
-					cc => $record->country_code,
-					latitude => $record->latitude,
-					longitude => $record->longitude,
-					state => $record->region,
-					city => $record->city,
+				if ($cc_only){
+					my $record = $geoip->country_code_by_addr($datum->{$key});
+					next unless $record;
+					$datum->{transforms}->{$Name}->{$key} = {
+						cc => $record
+					}
+				}
+				else {
+					my $record = $geoip->record_by_addr($datum->{$key});
+					next unless $record;
+					$datum->{transforms}->{$Name}->{$key} = {
+						cc => $record->country_code,
+						latitude => $record->latitude,
+						longitude => $record->longitude,
+						state => $record->region,
+						city => $record->city,
+					}
 				}
 			}
 		}
