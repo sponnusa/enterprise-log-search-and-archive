daaac7fb7a8e3a14d6c01f0617fd4f3bd156f0f4
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2012-12-13 17:05:22 +0000

    Added new version of count transform which returns unique values.

--- trunk/elsa/web/lib/Transform/Count.pm
+++ trunk/elsa/web/lib/Transform/Count.pm
@@ -9,19 +9,11 @@ extends 'Transform';
 our $Name = 'Count';
 # Whois transform plugin
 has 'name' => (is => 'rw', isa => 'Str', required => 1, default => $Name);
-has 'count' => (is => 'ro', isa => 'Num', required => 1, default => 0);
-has 'operator' => (is => 'ro', isa => 'Str', required => 1, default => '>=');
 
 sub BUILDARGS {
 	my $class = shift;
-	##my $params = $class->SUPER::BUILDARGS(@_);
 	my %params = @_;
-	if (defined $params{args}->[0]){
-		$params{count} = $params{args}->[0];
-	}
-	if ($params{args}->[1]){
-		$params{operator} = $params{args}->[1];
-	}
+	$params{groupby} = $params{args}->[0];
 	return \%params;
 }
 
@@ -29,7 +21,8 @@ sub BUILD {
 	my $self = shift;
 	$self->log->trace('data: ' . Dumper($self->data));
 	
-	DATUM_LOOP: foreach my $datum (@{ $self->data }){
+	my $sums = {};
+	foreach my $datum (@{ $self->data }){
 		foreach my $transform (keys %{ $datum->{transforms} }){
 			next unless ref($datum->{transforms}->{$transform}) eq 'HASH';
 			foreach my $transform_field (keys %{ $datum->{transforms}->{$transform} }){
@@ -37,71 +30,63 @@ sub BUILD {
 					if (exists $datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby }){
 						if (ref($datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby }) eq 'ARRAY'){
 							foreach my $value (@{ $datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } }){
-								my $test = $value . ' ' . $self->operator . ' ' . $self->count;
-								if (eval($test)){
-									#$self->log->trace('passed value ' . $value);
-									$datum->{transforms}->{$Name} = '__KEEP__';
-									next DATUM_LOOP;
-								}
+#								if ($value =~ /^\d+$/){
+#									$sums->{ $value } += $value;
+#								}
+#								else {
+#									$sums->{ $value } += $datum->{'count'};
+#								}
+								$sums->{ $value }++;
 							}
 						}
 						else {
-							if ($datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } =~ /^\d+$/){
-								my $test = $datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } . ' ' . $self->operator . ' ' . $self->count;
-								if (eval($test)){
-									#$self->log->trace('passed value ' . $value);
-									$datum->{transforms}->{$Name} = '__KEEP__';
-									next DATUM_LOOP;
-								}
-							}
+#							if ($datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } =~ /^\d+$/){
+#								$sums->{ $datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } } += 
+#									$datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby };
+#							}
+#							else {
+#								$sums->{ $datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } } += $datum->{count};
+#							}
+							$sums->{ $datum->{transforms}->{$transform}->{$transform_field}->{ $self->groupby } }++;
 						}
 					}
 				}
 				elsif (ref($datum->{transforms}->{$transform}->{$transform_field}) eq 'ARRAY' 
 					and $transform_field eq $self->groupby){
 					foreach my $value (@{ $datum->{transforms}->{$transform}->{$transform_field} }){
-						my $test = $value . ' ' . $self->operator . ' ' . $self->count;
-						if (eval($test)){
-							#$self->log->trace('passed value ' . $value);
-							$datum->{transforms}->{$Name} = '__KEEP__';
-							next DATUM_LOOP;
-						}
+#						if ($value =~ /^\d+$/){
+#							$sums->{ $value } += $value;
+#						}
+#						else {
+#							$sums->{ $value } += $datum->{'count'};
+#						}
+						$sums->{ $value }++;
 					}
 				}
 			}
 		}
 		if (exists $datum->{ $self->groupby } ){
-			my $test = $datum->{count} . ' ' . $self->operator . ' ' . $self->count;
-			#$self->log->trace('test: ' . $test);
-			if (eval($test)){
-				#$self->log->trace('passed value ' . $value);
-				$datum->{transforms}->{$Name} = '__KEEP__';
-				next DATUM_LOOP;
+			if ($datum->{ $self->groupby } =~ /^\d+$/){
+				#$sums->{ $self->groupby } += $datum->{ $self->groupby };
+				$sums->{ $self->groupby }++;
+			}
+			else {
+				#$sums->{ $datum->{ $self->groupby } } += $datum->{'count'};
+				$sums->{ $datum->{ $self->groupby } }++;
 			}
 		}
 	}
-	
+	$self->log->debug('sums: ' . Dumper($sums));
 	my $ret = [];
-		
-	my $count = scalar @{ $self->data };
-	for (my $i = 0; $i < $count; $i++){
-		if (exists $self->data->[$i]->{transforms}->{$Name}){
-			delete $self->data->[$i]->{transforms}->{$Name}; # no need to clutter our final results
-			push @$ret, { 
-				_groupby => $self->data->[$i]->{ $self->groupby }, 
-				intval => $self->data->[$i]->{count}, 
-				_count => $self->data->[$i]->{count},
-				count => $self->data->[$i]->{count},
-				$self->groupby => $self->data->[$i]->{ $self->groupby },
-			};
-		}
+	foreach my $key (keys %$sums){
+		push @$ret, { _groupby => $key, intval => $sums->{$key}, _count => $sums->{$key} };
 	}
 	
 	# Sort
 	$ret = [ sort { $b->{intval} <=> $a->{intval} } @$ret ];
+	
 	$self->data($ret);
-		
-	$self->log->debug('final data: ' . Dumper($self->data));
+	$self->log->debug('data: ' . Dumper($self->data));
 	
 	return $self;
 }
