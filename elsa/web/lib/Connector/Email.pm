e52cdf975ae2640a93bd92d3194a56890a6e8fed
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2013-02-01 19:25:23 +0000

    Added config item "email/include_data" to optionally include results in email alerts.

--- elsa/web/lib/Connector/Email.pm
+++ elsa/web/lib/Connector/Email.pm
@@ -32,13 +32,26 @@ sub BUILD {
 		From => $self->api->conf->get('email/display_address') ? $self->api->conf->get('email/display_address') : 'system',
 		Subject => $self->api->conf->get('email/subject') ? $self->api->conf->get('email/subject') : 'system',
 	};
-	my $body = sprintf('%d results for query %s', $self->query->results->records_returned, $self->query->query_string) .
-		"\r\n" . sprintf('%s/get_results?qid=%d&hash=%s', 
-			$self->api->conf->get('email/base_url') ? $self->api->conf->get('email/base_url') : 'http://localhost',
-			$self->query->qid,
-			$self->api->get_hash($self->query->qid),
-	);
-	
+	my $body;
+	if ($self->api->conf->get('email/include_data')){
+		if ($self->query->has_groupby){
+			$body = $self->query->results->TO_JSON();
+		}
+		else {
+			$body = 'Total Results: ' . $self->query->results->total_results . "\r\n";
+			foreach my $row ($self->query->results->all_results){
+				$body .= $row->{msg} . "\r\n";
+			}
+		}	
+	}
+	else {
+		$body = sprintf('%d results for query %s', $self->query->results->records_returned, $self->query->query_string) .
+			"\r\n" . sprintf('%s/get_results?qid=%d&hash=%s', 
+				$self->api->conf->get('email/base_url') ? $self->api->conf->get('email/base_url') : 'http://localhost',
+				$self->query->qid,
+				$self->api->get_hash($self->query->qid),
+		);
+	}
 	my ($query, $sth);
 	$query = 'SELECT UNIX_TIMESTAMP(last_alert) AS last_alert, alert_threshold FROM query_schedule WHERE id=?';
 	$sth = $self->api->db->prepare($query);
