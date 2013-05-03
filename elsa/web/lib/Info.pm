df36f43a65bbf710855f7892b607d1a9cfee23c4
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2013-05-03 20:01:50 +0000

    Added option to autoselect the correct StreamDB node based on configuration (#130)

--- elsa/web/lib/Info.pm
+++ elsa/web/lib/Info.pm
@@ -1,5 +1,6 @@
 package Info;
 use Moose;
+use Socket;
 # Base class for Info plugins
 has 'conf' => (is => 'rw', isa => 'Object', required => 1);
 has 'data' => (is => 'rw', isa => 'HashRef', required => 1, default => sub { {} });
@@ -9,9 +10,29 @@ has 'summary' => (is => 'rw', isa => 'Str', required => 1, default => '');
 
 sub BUILD {
 	my $self = shift;
-	if ($self->conf->get('streamdb_url')){
+	if ($self->conf->get('streamdb_urls')){
+		my $best_url;
+		URL_LOOP: foreach my $url (keys %{ $self->conf->get('streamdb_urls') }){
+			my $start_ip_int = unpack('N*', inet_aton($self->conf->get('streamdb_urls')->{$url}->{start}));
+			my $end_ip_int = unpack('N*', inet_aton($self->conf->get('streamdb_urls')->{$url}->{end}));
+			foreach my $col (qw(srcip dstip ip)){
+				if (exists $self->data->{$col}){
+					my $ip_int = unpack('N*', inet_aton($self->data->{$col}));
+					if ($start_ip_int <= $ip_int and $ip_int <= $end_ip_int){
+						$best_url = $url;
+						last URL_LOOP;
+					}
+				}
+			}
+		}
+		if ($best_url){
+			push @{ $self->plugins }, 'getStream_' . $best_url;
+		}
+	}
+	elsif ($self->conf->get('streamdb_url')){
 		push @{ $self->plugins }, 'getStream';
 	}
+	
 	if ($self->conf->get('pcap_url')){
 		push @{ $self->plugins }, 'getPcap';
 	}
