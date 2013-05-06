498ad9a9c650bb4dcf98093814dfb0247d7c4c33
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2013-05-06 19:41:09 +0000

    Added about/version panel and info

--- trunk/elsa/contrib/install.sh
+++ trunk/elsa/contrib/install.sh
@@ -941,8 +941,8 @@ mk_web_dirs(){
 
 set_version(){
 	# set ELSA version
-	svn info http://enterprise-log-search-and-archive.googlecode.com/svn/ | grep "Last Changed" | sed -e "s/Last Changed //g" | perl -e 'use Config::JSON; my $c = new Config::JSON("/etc/elsa_web.conf") or die($!); while(<>){ chomp; my ($k,$v) = split(/:/, $_); next unless $k and $v; $c->set("version/$k", $v); } $c->save;'
-	echo $SPHINX_VER | perl -e 'use Config::JSON; my $c = new Config::JSON("/etc/elsa_web.conf") or die($!); while(<>){ chomp; exit unless $_; $c->set("version/Sphinx", $_); } $c->save;'
+	svn info http://enterprise-log-search-and-archive.googlecode.com/svn/ | grep "Last Changed" | sed -e "s/Last Changed //g" | perl -e 'use Config::JSON; my $c = new Config::JSON("/etc/elsa_web.conf") or die($!); while(<>){ chomp; my ($k,$v) = split(/:/, $_); next unless $k and $v; $c->set("version/$k", $v); } $c->write;'
+	echo $SPHINX_VER | perl -e 'use Config::JSON; my $c = new Config::JSON("/etc/elsa_web.conf") or die($!); while(<>){ chomp; exit unless $_; $c->set("version/Sphinx", $_); } $c->write;'
 }
 
 suse_set_apache(){
