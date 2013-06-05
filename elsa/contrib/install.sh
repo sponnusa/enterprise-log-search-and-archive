3bf89a2e0c71ca3919f1fd6d166f1eeab35baa76
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2013-06-05 15:57:55 +0000

    Fixed wrong installer binary in install.sh (#149)

--- elsa/contrib/install.sh
+++ elsa/contrib/install.sh
@@ -827,7 +827,7 @@ ubuntu_get_web_packages(){
 	echo "debconf debconf/frontend select noninteractive" | debconf-set-selections
 	
 	if [ "$USE_LOCAL_MYSQL_PACKAGES" = 0 ]; then
-		zypper -qn install mysql-client libmysqlclient-dev
+		apt-get install mysql-client libmysqlclient-dev
 	fi
 	
 	# Install required packages
