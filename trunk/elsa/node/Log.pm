3742986fb056559f0f301f4d682dd3d33de4003e
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2013-06-10 21:05:13 +0000

    Fixed ops logs being misparsed due to square brackets, added debug destination for all logs to a unix domain socket and livetail.pl util for examining them

--- trunk/elsa/node/Log.pm
+++ trunk/elsa/node/Log.pm
@@ -28,6 +28,9 @@ no warnings;
 	
 	# Strip newlines and replace with a single space.
 	$message =~ s/[\n\r]+/\ /g;
+	
+	# Strip any undefs with square brackets
+	$message = s/\=\"\[undef\]\"/\=\"\"/g;
 
     $caller_level = 0 unless defined $caller_level;
 
@@ -64,7 +67,7 @@ sub config_logger {
 	
 	my $hostname = hostname;
 	
-	my $log_conf = qq{
+	my $log_conf = qq'
 		log4perl.category.App       = $log_level, $log_format
 		log4perl.appender.File			 = Log::Log4perl::Appender::File
 		log4perl.appender.File.filename  = $logdir/$logfile.log 
@@ -86,7 +89,7 @@ sub config_logger {
         log4perl.appender.RFC5424.Socket = $tmpdir/ops
         log4perl.appender.RFC5424.layout = Log::Log4perl::Layout::PatternLayout::Multiline
         log4perl.appender.RFC5424.layout.ConversionPattern = 1 %d{yyyy-MM-ddTHH:mm:ss.000}Z 127.0.0.1 elsa - 99 [elsa\@32473 priority="%p" method="%M" file="%F{2}" line_number="%L" pid="%P" hostname="$hostname"] %m%n
-	};
+	';
 	
 	if (not Log::Log4perl->initialized()){
 		Log::Log4perl::init( \$log_conf ) or die("Unable to init logger");
