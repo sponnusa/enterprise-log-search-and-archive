c12b458d29bf78d23bf9db4c52cc1710456a94b5
mcholste <mcholste@a444dacd-7e8a-212c-b5c1-8dde9c117f55>
2014-03-23 22:46:44 +0000

    Added preference and default settings for log_size_limit and rows_per_page

--- 1.5/web/lib/Query.pm
+++ 1.5/web/lib/Query.pm
@@ -117,6 +117,15 @@ sub BUILD {
 		}
 	}
 	
+	# Override default limit if one is given in the config file
+	if (defined $self->conf->get("default_results_limit")){
+		$self->limit($self->conf->get("default_results_limit"));
+	}
+	# Override that with user preference
+	if (defined $self->user->preferences->{tree}->{default_settings}->{limit}){
+		$self->limit($self->user->preferences->{tree}->{default_settings}->{limit});
+	}
+	
 	# Map directives to their properties
 	foreach my $prop (keys %{ $self->parser->directives }){
 		$self->$prop($self->parser->directives->{$prop});
